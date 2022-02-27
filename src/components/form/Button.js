c7fee8efadd9d824fd30d0a455cc54cdf2d0f7bc
BAHA Ephraïm Jean-Samuel <jeansamist@gmail.com>
2022-02-27 05:26:02 +0100

    init files

---
+++ src/components/form/Button.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Button() {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  );
+}
